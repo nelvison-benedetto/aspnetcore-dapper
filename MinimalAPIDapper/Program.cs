7e81d3d0d2a9552c23a9d5d9f5f04ec4eb4cbbc7
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-13 23:55:39 +0100

    update

--- MinimalAPIDapper/Program.cs
+++ MinimalAPIDapper/Program.cs
@@ -1,6 +1,26 @@
+using MinimalAPIDapper.Entities;
+
+//###services zone
+
 var builder = WebApplication.CreateBuilder(args);
+
+//###
+
 var app = builder.Build();
 
+//###middleware zone
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/genres", () =>
+{
+    var genres = new List<Genre>
+    {
+        new Genre { Id = 1, Name = "Action" },
+        new Genre { Id = 2, Name = "Comedy" },
+        new Genre { Id = 3, Name = "Drama" }
+    };
+    return genres;
+});
+//###
 
 app.Run();
